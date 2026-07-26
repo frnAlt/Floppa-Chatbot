1a96fc45039d9a6d86c698309bc575e070fcc0a5
Gtajisan <gtajisan@floppa.chat>
2026-07-26 09:50:58 +0000

    rebrand: update metadata, 2D mascot banner, README & credits to Gtajisan (Farhan Muh Tasim)
    Floppa-Chatbot developed & maintained by Gtajisan (Farhan Muh Tasim)

--- languages/cmds/en.js
+++ languages/cmds/en.js
@@ -345,10 +345,10 @@ module.exports = {
 		description: "View command usage",
 		guide: "{pn} [empty | <page number> | <command name>]",
 		text: {
-			help: "╭─────────────⭓\n%1\n├─────⭔\n│ Page [ %2/%3 ]\n│ Currently, the bot has %4 commands that can be used\n│ » Type %5help <page> to view the command list\n│ » Type %5help to view the details of how to use that command\n├────────⭔\n│ %6\n╰─────────────⭓",
-			help2: "%1├───────⭔\n│ » Currently, the bot has %2 commands that can be used\n│ » Type %3help <command name> to view the details of how to use that command\n│ %4\n╰─────────────⭓",
+			help: "Commands:\n%1\nPage %2/%3 • Total: %4\n- Use %5help <page> to list\n- Use %5help <command> for details\n%6",
+			help2: "%1Total: %2\n- Use %3help <command> for details\n%4",
 			commandNotFound: "Command \"%1\" does not exist",
-			getInfoCommand: "╭── NAME ────⭓\n│ %1\n├── INFO\n│ Description: %2\n│ Other names: %3\n│ Other names in your group: %4\n│ Version: %5\n│ Role: %6\n│ Time per command: %7s\n│ Author: %8\n├── Usage\n%9\n├── Notes\n│ The content inside <XXXXX> can be changed\n│ The content inside [a|b|c] is a or b or c\n╰──────⭔",
+			getInfoCommand: "Name: %1\nDescription: %2\nAliases: %3\nGroup aliases: %4\nVersion: %5\nRole: %6\nCooldown: %7s\nAuthor: %8\n\nUsage:\n%9\n\nNotes:\n- <> = required, [] = choose one",
 			doNotHave: "Do not have",
 			roleText0: "0 (All users)",
 			roleText1: "1 (Group administrators)",
